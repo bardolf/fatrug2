f6ac144ea4d62822aec03a144b869a15fd0c950f
Milan Nemec <milanemec@gmail.com>
2022-08-23 18:34:00 +0200

    comment on battery capacity

--- src/battery.cpp
+++ src/battery.cpp
@@ -2,6 +2,14 @@
 
 #define ANALOG_BATTERY_PIN 36
 
+/*
+Battery measurement 
+read analog value >> 4 (highest 8 bits) gives us a number 0-255.
+
+Because of different ADC and resistors, we have 127/125 for 3.6V. 
+The device stops to work on the value 127.
+*/
+
 Battery::Battery() {
 }
 
