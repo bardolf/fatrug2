9b5a65b0b6f4a441eb7c86255d28c3c2584f0a69
Milan Nemec <milanemec@gmail.com>
2022-08-24 15:23:08 +0200

    comment on battery capacity

--- src/battery.cpp
+++ src/battery.cpp
@@ -8,6 +8,18 @@ read analog value >> 4 (highest 8 bits) gives us a number 0-255.
 
 Because of different ADC and resistors, we have 127/125 for 3.6V. 
 The device stops to work on the value 127.
+
+Table with my battery observations
+        1(Red)        2(White)
+USB disconnected, battery (discharged - 3.6V) connected
+        127         125
+USB connected, battery disconnected
+        148+        142+
+USB connected, battery (discharged) connected 
+        139         134
+USB disconnected, battery (charged - 4.15 / 4.13V) connected
+        147         146 
+
 */
 
 Battery::Battery() {
